e979912ac1ff3b6bdddbfc860dbd8290c1695da6
Juan Cassiano <juancassiano@hotmail.com>
2022-07-21 15:09:49 -0300

    Create Sticker

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +11,8 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_27f42loh";
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_27f42loh";
+        String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies"; // MOCK
         URI address = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(address).GET().build();
@@ -21,8 +24,16 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
+            String urlImage = filme.get("image");
+            String title = filme.get("title");
+
+            InputStream inputStream = new URL(urlImage).openStream();
+
+            String fileName = title + ".png";
+            StickersGenerator stickersGenerator = new StickersGenerator();
+            stickersGenerator.create(inputStream, fileName);
+
+            System.out.println(title);
             System.out.println(filme.get("imDbRating"));
             System.out.println();
         }
---
+++ src/StickersGenerator.java
@@ -0,0 +1,37 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.imageio.ImageIO;
+
+public class StickersGenerator {
+
+  public void create(InputStream inputStream, String fileName) throws Exception {
+    // InputStream inputStream = new FileInputStream(new File("assets/wololo.jpg"));
+    // InputStream inputStream = new
+    // URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_2.jpg").openStream();
+    BufferedImage originalImage = ImageIO.read(inputStream);
+
+    int width = originalImage.getWidth();
+    int height = originalImage.getHeight();
+    int newHeight = height + 200;
+
+    BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+
+    Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+    graphics.drawImage(originalImage, 0, 0, null);
+
+    Font font = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+    graphics.setColor(Color.YELLOW);
+    graphics.setFont(font);
+
+    graphics.drawString("IMERSAO JAVA ALURA", 100, newHeight - 100);
+
+    ImageIO.write(newImage, "png", new File("assets" + fileName));
+
+  }
+
+}
